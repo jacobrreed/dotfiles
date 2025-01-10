17ae13f69ac8f0493eeb0345a9e15ea18a5a7270
jacobrreed <jacob.russell.reed@gmail.com>
2025-01-10 09:52:42 -0500

    upd

--- .config/pacman/postpacman.sh
+++ .config/pacman/postpacman.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup component add rust-analyzer
+rustup default nightly
+# bat
+bat cache --build
 
-# Services
-sudo systemctl enable greetd --now
-sudo systemctl enable bluetooth --now
-sudo systemctl enable keyd --now
-sudo systemctl enable paccache.timer --now
-systemctl --user enable pipewire --now
+# Hyprland services (if using UWSM)
+systemctl --user enable --now hypridle.service
+systemctl --user enable --now hyprpolkitagent
