c9c4f71319b943e8222e6e646fdde91d2fa850df
Jacob Reed <jacob.russell.reed@gmail.com>
2025-03-18 13:54:14 -0400

    Update user.js

--- .mozilla/firefox/user.js
+++ .mozilla/firefox/user.js
@@ -236,6 +236,7 @@ user_pref("layout.word_select.eat_space_to_next_word", false);
 // Enter your personal overrides below this line:
 user_pref("network.trr.mode", 5);
 user_pref("sidebar.verticalTabs", true);
+user_pref("sidebar.visibility", "show");
 user_pref("media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled", true);
 user_pref("browser.toolbars.bookmarks.visibility", "always");
 user_pref("services.sync.prefs.sync-seen.browser.urlbar.showSearchSuggestionsFirst", false);
